6191edb11bf4ed6b026869a2bd7062db683ddb36
Viktar Hrynko <vhrynko@infoblox.com>
2024-07-15 16:43:02 +0300

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Import the Express library
 const express = require('express');
+const cors = require('cors');
 
 // Create an instance of an Express application
 const app = express();
 
 // Define the port the server will listen on
 const port = 3000;
+
+app.use(cors());
 
 // Define a route for the /hello endpoint
 app.get('/hello', (req, res) => {
